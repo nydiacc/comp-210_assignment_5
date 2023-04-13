ed6b30a5ac5403d086840fac275e54f169ae4d36
Nydia Cabrera <nydiac@unc.edu>
2023-04-12 21:20:13 -0400

    commit four

--- a5/Edge.java
+++ a5/Edge.java
@@ -7,7 +7,7 @@ public interface Edge {
     /*Hint: Make sure you  update the Edge interface in Edge.java when you add a new method implementation
     in EdgeImpl.java, and vice-versa.  getName() in Node.java and NodeImpl.java is an example.  Also, files in
     previous homeworks (e.g., BST.java and BSTImpl.java in homework 3) are good examples of
-    interfaces and their implementations.
+    interfaces and their  implementations.
      */
     Node getSrc();
     Node getDest();
